3ac5f3f340607c62225701ee6e22f6a927bceacd
Paloma Souza <palomalimeira@gmail.com>
2021-10-21 13:15:17 -0300

    Changing index aligment

--- pages/index.js
+++ pages/index.js
@@ -5,7 +5,7 @@ import Image from 'next/image'
 
 export default function Home() { //página de início apenas com um link para a página de filmes
   return (
-    <div className="container">
+    <div className="container" style={{ display: "flex", justifyContent: "center",}}>
       <Head>
         <title>SW Challenge</title>
       </Head>
@@ -14,7 +14,7 @@ export default function Home() { //página de início apenas com um link para a 
           <p>Bem vindos ao primeiro desafio</p>
         </h2>
         <Link href="/films">
-          <a><Image src={"https://logodownload.org/wp-content/uploads/2015/12/star-wars-logo-3-1.png"} className="sw-logo" alt="logo" width="300"  height="145"/></a>
+          <a><Image src={"https://logodownload.org/wp-content/uploads/2015/12/star-wars-logo-3-1.png"} className="sw-logo" alt="logo" width="580"  height="250"/></a>
         </Link>
         <p>
             Clique no logo para iniciar
